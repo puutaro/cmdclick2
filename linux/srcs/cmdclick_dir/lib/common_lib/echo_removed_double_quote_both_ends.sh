8dc3daae0aed69f1f587232f5c5872fae46124cb
puutaro <hoperstep@gmail.com>
2026-09-05 20:39:21 +0900

    sed to bash batter tikan

--- linux/srcs/cmdclick_dir/lib/common_lib/echo_removed_double_quote_both_ends.sh
+++ linux/srcs/cmdclick_dir/lib/common_lib/echo_removed_double_quote_both_ends.sh
@@ -3,9 +3,14 @@
 
 echo_removed_double_quote_both_ends(){
 	local remove_contents="${1}"
-	echo "${remove_contents}" \
-		| sed -r -e 's/^"(.*)"$/\1/' \
-			-e "s/^'(.*)'$/\1/"
+#	echo "${remove_contents}" \
+#		| sed -r -e 's/^"(.*)"$/\1/' \
+#			-e "s/^'(.*)'$/\1/"
+  remove_contents="${remove_contents#\"}"
+  remove_contents="${remove_contents%\"}"
+  remove_contents="${remove_contents#\'}"
+  remove_contents="${remove_contents%\'}"
+  echo "${remove_contents}"
 }
 
 echo_removed_double_quote_both_ends_from_pip(){
