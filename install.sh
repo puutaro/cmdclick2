f4df584f8bc67d48a888dda6eaa5750b8489a20c
puutaro <hoperstep@gmail.com>
2026-09-06 18:01:35 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+readonly OS_TYPE="$(uname -s)"
+
+if [ "${OS_TYPE}" = "Darwin" ]; then
+  curl -fsSL "https://raw.githubusercontent.com/puutaro/cmdclick2/refs/heads/master/install/mac/installer.sh" | bash
+else
+  curl -fsSL "https://raw.githubusercontent.com/puutaro/cmdclick2/refs/heads/master/install/linux/installer.sh" | bash
+fi
