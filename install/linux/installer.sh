f3c4d5abbf9bbdb2c7ca1961bf153db3d924faaa
puutaro <55217593+puutaro@users.noreply.github.com>
2026-09-06 18:33:43 +0900

    Update Sublime Text installation method in installer.sh
    Removed old Sublime Text configuration and added new APT method for installation.

--- install/linux/installer.sh
+++ install/linux/installer.sh
@@ -7,11 +7,15 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y yad wmctrl x11-xserver-utils xdotool xclip wget curl gnupg
 
 # install sublime (Official APT method
-sudo rm -f /etc/apt/sources.list.d/sublime-text.list
-sudo rm -f /etc/apt/sources.list.d/sublime-text.sources
-sudo rm -f /etc/apt/trusted.gpg.skel
+# 競合する古い設定の完全削除（sources.list および sources.list.d 内）
+sudo sed -i '/sublimetext\.com/d' /etc/apt/sources.list
 sudo rm -f /etc/apt/sources.list.d/sublime-text*
-sudo find /etc/apt/ -type f -name "*.list" -exec grep -l "sublimetext.com" {} \; 2>/dev/null
+sudo rm -f /etc/apt/trusted.gpg.skel
+
+# install sublime (Official APT method)
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e "Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc" | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 
 sudo install -d -m 0755 /etc/apt/keyrings
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
