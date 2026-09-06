15d14e95eae7bed9c987cf9bf32702ce3c3bfeba
puutaro <55217593+puutaro@users.noreply.github.com>
2026-09-06 18:14:36 +0900

    Refactor Sublime Text installation in installer.sh
    Updated the installation method for Sublime Text to use the official APT method with keyrings.

--- install/linux/installer.sh
+++ install/linux/installer.sh
@@ -6,9 +6,10 @@ set -ue
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y yad wmctrl x11-xserver-utils xdotool xclip wget curl gnupg
 
-# install sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.skel > /dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.skel] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# install sublime (Official APT method)
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e "Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc" | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt-get update -y
 sudo apt-get install sublime-text -y
 
