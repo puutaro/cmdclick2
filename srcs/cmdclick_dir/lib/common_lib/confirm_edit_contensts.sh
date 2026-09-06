7dc3cd7478683b2b4407c5509824f87c9f9bc5f2
puutaro <hoperstep@gmail.com>
2026-09-07 00:31:25 +0900

    remove yad escaper

--- srcs/cmdclick_dir/lib/common_lib/confirm_edit_contensts.sh
+++ srcs/cmdclick_dir/lib/common_lib/confirm_edit_contensts.sh
@@ -4,14 +4,15 @@
 #yad用入力値反映イニファイル内容を作成
 confirm_edit_contensts(){
 	local LANG="ja_JP.UTF-8" 
-	local display_ini_contents=$(\
-	echo  "${1}" \
-		| sed -e 's/\&/\&amp;/g' \
-		 -e 's/</\&lt;/g' \
-		 -e 's/>/\&gt;/g' \
-		 -e 's/"/\&quot;/g' \
-		 -e "s/'/\&apos;/g" \
-	)
+	local display_ini_contents="${1}"
+#	local display_ini_contents=$(\
+#	echo  "${1}" \
+#		| sed -e 's/\&/\&amp;/g' \
+#		 -e 's/</\&lt;/g' \
+#		 -e 's/>/\&gt;/g' \
+#		 -e 's/"/\&quot;/g' \
+#		 -e "s/'/\&apos;/g" \
+#	)
 	local save_confirm_message="\n Do you really want to save bellow ini file ? \n"
 	set +e
 	guigui \
