4e6850c4f1241d0645e9d97fd445bedef5f0c27e
puutaro <hoperstep@gmail.com>
2026-09-07 00:37:03 +0900

    rename proc fastly

--- srcs/cmdclick_dir/lib/edit_lib/edit_ini_file_lib/echo_ini_contents_moto.sh
+++ srcs/cmdclick_dir/lib/edit_lib/edit_ini_file_lib/echo_ini_contents_moto.sh
@@ -5,13 +5,15 @@ echo_ini_contents_moto(){
 	local ini_contents="${2}"
 	local edit_file_name="${3}"
 	case "${roop_num}" in  
-		"1") 
-			local sed_edit_file_name=$(\
-					echo "${edit_file_name}" \
-						| sed -r 's/([^a-zA-Z0-9_-])/\\\1/g' \
-				)
-			cat "${EDIT_FILE_PATH}" \
-				| sed "s/^${INI_CMD_FILE_NAME}=.*/${INI_CMD_FILE_NAME}=${sed_edit_file_name}/"
+		"1")
+		local sed_edit_file_name="${edit_file_name//[^a-zA-Z0-9_-]/\\&}"
+    sed "s/^${INI_CMD_FILE_NAME}=.*/${INI_CMD_FILE_NAME}=${sed_edit_file_name}/" "${EDIT_FILE_PATH}"
+#			local sed_edit_file_name=$(\
+#					echo "${edit_file_name}" \
+#						| sed -r 's/([^a-zA-Z0-9_-])/\\\1/g' \
+#				)
+#			cat "${EDIT_FILE_PATH}" \
+#				| sed "s/^${INI_CMD_FILE_NAME}=.*/${INI_CMD_FILE_NAME}=${sed_edit_file_name}/"
 			return
 	;; esac
     echo "${ini_contents}"
