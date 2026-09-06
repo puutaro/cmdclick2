5edafb8fdd6d9828afece1527d1758d4fbfbbb1d
puutaro <55217593+puutaro@users.noreply.github.com>
2026-09-06 18:26:46 +0900

    Update Sublime Text installation script
    Remove existing Sublime Text source lists and add new keyring and source.

--- install/linux/installer.sh
+++ install/linux/installer.sh
@@ -10,6 +10,9 @@ sudo apt-get install -y yad wmctrl x11-xserver-utils xdotool xclip wget curl gnu
 sudo rm -f /etc/apt/sources.list.d/sublime-text.list
 sudo rm -f /etc/apt/sources.list.d/sublime-text.sources
 sudo rm -f /etc/apt/trusted.gpg.skel
+sudo rm -f /etc/apt/sources.list.d/sublime-text*
+sudo find /etc/apt/ -type f -name "*.list" -exec grep -l "sublimetext.com" {} \; 2>/dev/null
+
 sudo install -d -m 0755 /etc/apt/keyrings
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
 echo -e "Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc" | sudo tee /etc/apt/sources.list.d/sublime-text.sources
