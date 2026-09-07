8055d10adfdcc50277bb5971310e2c026c10cc90
puutaro <hoperstep@gmail.com>
2026-09-07 23:00:02 +0900

    index fastly

--- srcs/cmdclick_dir/lib/common_lib/echo_longpath_by_summraizing.sh
+++ srcs/cmdclick_dir/lib/common_lib/echo_longpath_by_summraizing.sh
@@ -1,25 +1,59 @@
 #!/usr/bin/env bash
 
-
 echo_longpath_by_summraizing(){
-	local path="${1}"
+	local target_path="${1}"
 	local display_path_hierarchy_limit_num=4
-	echo "${path}" \
-		| awk \
-		-v HOME="${HOME}" \
-		-v display_path_hierarchy_limit_num=${display_path_hierarchy_limit_num} \
-		'{
-			target_path = $0
-			gsub(HOME, "~", target_path)
-			target_path_list_length = split(\
-				target_path, \
-				target_path_list, \
-				"/" \
-			)
-			if(target_path_list_length <= display_path_hierarchy_limit_num){
-				print target_path
-				exit
-			}
-			print target_path_list[1]"/../"target_path_list[target_path_list_length-1]"/"target_path_list[target_path_list_length]
-		}'
+
+	# 1. $HOME を ~ に置換 (先頭にマッチした場合のみ置換)
+	if [[ "${target_path}" == "${HOME}"* ]]; then
+		target_path="~${target_path#"${HOME}"}"
+	fi
+
+	# 2. パスをスラッシュ / で分解して配列に格納 (空要素を除外するため shopt 設定)
+	local target_path_list
+	IFS='/' read -r -a target_path_list <<< "${target_path}"
+
+	# 階層数 (配列の要素数) を取得
+	local target_path_list_length="${#target_path_list[@]}"
+
+	# 階層数が制限以下の場合はそのまま出力
+	if [ "${target_path_list_length}" -le "${display_path_hierarchy_limit_num}" ]; then
+		echo "${target_path}"
+		return 0
+	fi
+
+	# 3. 制限を超えている場合: target_path_list[1] /../ 後半2つ を結合
+	# (インデックス: 0は先頭の空要素、1は最初の要素、長さ-2 は後ろから2番目の要素)
+	local first_elem="${target_path_list[0]}"
+	# パスが絶対パス（/で始まる）か相対パス（~で始まる）かで最初の要素を取得
+	if [ -z "${first_elem}" ] && [ "${target_path_list_length}" -gt 1 ]; then
+		first_elem="${target_path_list[1]}"
+	fi
+
+	local prev_last_elem="${target_path_list[$((target_path_list_length - 2))]}"
+	local last_elem="${target_path_list[$((target_path_list_length - 1))]}"
+
+	echo "${first_elem}/../${prev_last_elem}/${last_elem}"
 }
+#echo_longpath_by_summraizing(){
+#	local path="${1}"
+#	local display_path_hierarchy_limit_num=4
+#	echo "${path}" \
+#		| awk \
+#		-v HOME="${HOME}" \
+#		-v display_path_hierarchy_limit_num=${display_path_hierarchy_limit_num} \
+#		'{
+#			target_path = $0
+#			gsub(HOME, "~", target_path)
+#			target_path_list_length = split(\
+#				target_path, \
+#				target_path_list, \
+#				"/" \
+#			)
+#			if(target_path_list_length <= display_path_hierarchy_limit_num){
+#				print target_path
+#				exit
+#			}
+#			print target_path_list[1]"/../"target_path_list[target_path_list_length-1]"/"target_path_list[target_path_list_length]
+#		}'
+#}
--- srcs/cmdclick_dir/lib/common_lib/input_gui_lib/echo_ini_file_list.sh
+++ srcs/cmdclick_dir/lib/common_lib/input_gui_lib/echo_ini_file_list.sh
@@ -3,23 +3,50 @@
 echo_ini_file_list(){
 	local ini_file_dir_path="${1}"
 	local display_ini_file_dir_path="${2}"
-	ls -tl "${ini_file_dir_path}" \
-	| awk \
-		-v ini_file_dir_path="${ini_file_dir_path}" \
-		-v display_ini_file_dir_path="${display_ini_file_dir_path}" \
-		'
-		BEGIN {
-			print "["display_ini_file_dir_path"]"
-			display_count = 0
-		}
-		{
-			nine_field_later_str=substr($0, index($0, $9), length($0))
-			gsub(/\*$/, "", nine_field_later_str)
-			if(!match(nine_field_later_str, /\.sh$/)) next
-			print nine_field_later_str"\t"ini_file_dir_path
-			display_count++
-		}
-		END {
-			if(!display_count) print "-\t"ini_file_dir_path
-		}'
+
+	# [ヘッダー] の出力
+	echo "[${display_ini_file_dir_path}]"
+
+	local file_name
+	local display_count=0
+
+	# ls -1t で更新日時順にファイル名（またはパス）だけを高速取得
+	while IFS= read -r file_name; do
+		# *.sh 以外（またはファイルが存在しない場合）はスキップ
+		[[ "${file_name}" == *.sh ]] || continue
+
+		# 末尾の * を削除 (ls -F 対策)
+		file_name="${file_name%\*}"
+
+		echo -e "${file_name}\t${ini_file_dir_path}"
+		display_count=$((display_count + 1))
+	done < <(ls -1t "${ini_file_dir_path}" 2>/dev/null)
+
+	# 1件もヒットしなかった場合
+	if [ "${display_count}" -eq 0 ]; then
+		echo -e "-\t${ini_file_dir_path}"
+	fi
 }
+#echo_ini_file_list(){
+#	local ini_file_dir_path="${1}"
+#	local display_ini_file_dir_path="${2}"
+#	ls -tl "${ini_file_dir_path}" \
+#	| awk \
+#		-v ini_file_dir_path="${ini_file_dir_path}" \
+#		-v display_ini_file_dir_path="${display_ini_file_dir_path}" \
+#		'
+#		BEGIN {
+#			print "["display_ini_file_dir_path"]"
+#			display_count = 0
+#		}
+#		{
+#			nine_field_later_str=substr($0, index($0, $9), length($0))
+#			gsub(/\*$/, "", nine_field_later_str)
+#			if(!match(nine_field_later_str, /\.sh$/)) next
+#			print nine_field_later_str"\t"ini_file_dir_path
+#			display_count++
+#		}
+#		END {
+#			if(!display_count) print "-\t"ini_file_dir_path
+#		}'
+#}
